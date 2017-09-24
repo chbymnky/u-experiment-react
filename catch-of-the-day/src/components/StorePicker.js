6986a28e4754113a7039fb01c907bdfac45aba7a
Myron Schippers <mschippe@nerdery.com>
2017-09-24 16:20:46 -0500

    12 - All About React Router, using ContextTypes in react

--- catch-of-the-day/src/components/StorePicker.js
+++ catch-of-the-day/src/components/StorePicker.js
@@ -12,8 +12,10 @@ class StorePicker extends React.Component {
         event.preventDefault();
         console.log('You changed the URL');
         // first grab the text from the box
-        console.log(this.storeInput.value);
+        const storeId = this.storeInput.value;
+        console.log(`Going to ${storeId}`);
         // second we're going to transition from / to /store/:storeId
+        this.context.router.transitionTo(`/store/${storeId}`);
     }
 
     render() {
@@ -27,5 +29,9 @@ class StorePicker extends React.Component {
         );
     }
 }
+
+StorePicker.contextTypes = {
+    router: React.PropTypes.object
+};
 
 export default StorePicker;
