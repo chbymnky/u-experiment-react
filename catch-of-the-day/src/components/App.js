09280d4e6e039088f8c246e2b02641d936efec30
Myron Schippers <mschippe@nerdery.com>
2017-09-24 17:23:13 -0500

    episode 13 - Understanding State, managing state with React only no Redux of Flux is used to manage state

--- catch-of-the-day/src/components/App.js
+++ catch-of-the-day/src/components/App.js
@@ -4,15 +4,37 @@ import Order from './Order';
 import Inventory from './Inventory';
 
 class App extends React.Component {
+    constructor() {
+        super();
+
+        this.addFish = this.addFish.bind(this);
+        // getinitialState
+        this.state = {
+            fishes: {},
+            order: {},
+        };
+    }
+
+    addFish(fish) {
+        // update our state
+        const fishes = {...this.state.fishes};
+        // add in our new fish
+        const timestamp = Date.now();
+
+        fishes[`fish-${timestamp}`] = fish;
+
+        // set state
+        this.setState({ fishes }); // same as { fishes: fishes }
+    }
+
     render() {
         return (
             <div className="catch-of-the-day">
                 <div className="menu">
-                    <Header
-                        tagline="Fresh Seafood Market" />
+                    <Header tagline="Fresh Seafood Market" />
                 </div>
                 <Order />
-                <Inventory />
+                <Inventory addFish={this.addFish} />
             </div>
         );
     }
