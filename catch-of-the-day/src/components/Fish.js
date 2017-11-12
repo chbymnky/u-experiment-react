b84ffeb6be939a1c38bb62bc8bfb8a680b7403ab
Myron R Schippers Jr <chbymnky@gmail.com>
2017-11-11 22:32:24 -0600

    Episode 23 - Component Validation with PropTypes, define property data types for all components in the application

--- catch-of-the-day/src/components/Fish.js
+++ catch-of-the-day/src/components/Fish.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { PropTypes } from 'react';
 import { formatPrice } from '../helpers';
 
 class Fish extends React.Component {
@@ -19,6 +19,12 @@ class Fish extends React.Component {
             </li>
         );
     }
+}
+
+Fish.proptypes = {
+    details: PropTypes.object.isRequired,
+    index: PropTypes.string.isRequired,
+    addToOrder: PropTypes.func.isRequired,
 }
 
 export default Fish;
